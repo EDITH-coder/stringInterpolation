36a3123ea05c858f3ef575faa0ac929d18761a9c
EDITH_Coder <edithmashapo@gmail.com>
2023-10-16 05:05:48 +0200

    Add project files.

---
+++ stringInterpolation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringInterpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Enter your Name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your lastName: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter you age: ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Hi, {firstName} {lastName} you are {Age} years old");
+
+            Console.ReadLine();
+
+        }
+    }
+}
